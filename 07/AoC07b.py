89f024afc15110f4e52fffc9d6a7fdabe9d28ca5
Jacco Hoekstra <jaccohoekstra@tudelft.nl>
2024-12-07 17:38:05 +0100

    Day 7
    Vandaag, dag 7, was makkelijker dan gisteren, vond ik.

---
+++ 07/AoC07b.py
@@ -0,0 +1,40 @@
+goals = []
+termen = []
+
+# Read data and make tables
+#f = open("test.txt","r")
+f = open("input.txt","r")
+for line in f.readlines():
+    goaltxt,somtxt = line.split(":")
+    goals.append(int(goaltxt))
+    termtxt = somtxt.replace("\n","").split()
+    row = []
+    for txt in termtxt:
+        row.append(int(txt))
+    termen.append(row)
+f.close()
+
+# Initialisation
+total = 0 # sum of correct outcomes
+N = len(goals)
+
+# Try all problems one by one
+
+for i in range(N):
+    results = [termen[i][0]] # list to keep potential outcomes
+
+    #for each temrm calculate two offsprings in next generation results
+    for term in termen[i][1:]:
+        newresults = []
+        for result in results:
+            newresults.append(result*term)
+            newresults.append(result+term)
+            newresults.append(int(str(result)+str(term)))
+        results = newresults.copy()
+    if goals[i] in results:
+        total = total + goals[i]
+
+print("Total =",total)
+
+            
+        
